cfa54f096659106b9926d3073332667c1ad5611d
ChinaNation <chinanation@icloud.com>
2026-03-09 21:06:53 -0700

    更新

--- citizenchain/nodeui/backend/src/settings/mod.rs
+++ citizenchain/nodeui/backend/src/settings/mod.rs
@@ -1,6 +1,6 @@
+pub(crate) mod address_utils;
 #[path = "bootnodes-address/mod.rs"]
 pub mod bootnodes_address;
-pub(crate) mod address_utils;
 #[path = "fee-address/mod.rs"]
 pub mod fee_address;
 #[path = "grandpa-address/mod.rs"]
